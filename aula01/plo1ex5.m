ac723dea4b41fc05bd345a6438a04339ec0dca25
jmnmv12 <j.vasconcelos99@gmail.com>
2018-09-25 18:09:10 +0100

    Bug fix

--- aula01/plo1ex5.m
+++ aula01/plo1ex5.m
@@ -1,5 +1,6 @@
 %%Código a)
-
+close all;
+clear all;
 N=1e6;
 n=5;%amostra de 5 torneiras
 p=0.3;%o.3 de prob de ter defeito
@@ -7,6 +8,9 @@ k=3; %3 torneiras com defeito
 
 prob=calcp(p,k,n,N);
 fprintf(1,'P(%d peças defeituosas em %d peças)=%.4f\n',k,n,prob)
+
+prob_analitica= factorial(n)/(factorial(n-k)*factorial(k))*p^k*(1-p)^(n-k)
+
 
 %%Código b)
 clear;
@@ -35,4 +39,8 @@ for k=0:n
     
 end
 
-hist(prob)
+figure(1);
+bar(1:6,prob),colormap(cool)
+
+figure(2);
+hist(prob,5)
